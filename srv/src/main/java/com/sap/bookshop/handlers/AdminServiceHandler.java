1b7de7401fe4507e4c1a97d0ad27046fee0bd154
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-07-25 12:53:08 +0530

    Create AdminServiceHandler.java

---
+++ srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
@@ -0,0 +1,29 @@
+package com.sap.bookshop.handlers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.Result;
+import com.sap.cds.services.cds.CdsReadEventContext;
+import com.sap.cds.services.cds.CqnService;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+
+import cds.gen.bookshop1.Books_;
+import cds.gen.adminservice.AdminService_;
+@Component
+@ServiceName(AdminService_.CDS_NAME)
+public class AdminServiceHandler implements EventHandler{
+
+    @Autowired
+    @Qualifier(Books_.CDS_NAME)
+    CqnService bupa;
+
+     @On(entity = Books_.CDS_NAME)
+  Result readSuppliers(CdsReadEventContext context) {
+    return bupa.run(context.getCqn());
+  }
+
+}
