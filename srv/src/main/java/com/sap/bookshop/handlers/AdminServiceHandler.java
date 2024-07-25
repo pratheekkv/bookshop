04510f12110ad8d1fb0620c9cddbf84293430686
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-07-25 23:39:24 +0530

    Update AdminServiceHandler.java

--- srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
@@ -1,18 +1,24 @@
 package com.sap.bookshop.handlers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.Result;
+import com.sap.cds.ql.Select;
+import com.sap.cds.ql.cqn.CqnSelect;
 import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import static com.sap.cds.ResultBuilder.selectedRows;
 
 import cds.gen.bookshop1.Books_;
+import cds.gen.bookshop1.Bookshop1;
 import cds.gen.bookshop1.Bookshop1_;
 import cds.gen.adminservice.AdminService_;
 import cds.gen.adminservice.ExternalBooks_;
@@ -27,17 +33,11 @@ public class AdminServiceHandler implements EventHandler{
     @Autowired
     private ApplicationContext applicationContext;
 
-     @On(entity = ExternalBooks_.CDS_NAME)
-  Result readSuppliers(CdsReadEventContext context) {
-    String[] beanNames = applicationContext.getBeanNamesForType(CqnService.class);
-
-    // Print each bean name
-    System.out.println("Beans of type CqnService:");
-    for (String beanName : beanNames) {
-        System.out.println(beanName);
-    }
-    // return null;
-    return bupa.run(context.getCqn());
+    @On(entity = ExternalBooks_.CDS_NAME)
+    Result readSuppliers(CdsReadEventContext context) {
+     CqnSelect select = Select.from(Bookshop1_.CDS_NAME).limit(100);
+     List<Bookshop1> businessPartner = bupa.run(select).listOf(Bookshop1.class);
+     return null;
   }
 
 }
