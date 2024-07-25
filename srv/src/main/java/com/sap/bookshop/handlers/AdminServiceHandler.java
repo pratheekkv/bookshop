f7d25f39278644a9196605c10932c7d3b59cd572
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-07-25 23:29:57 +0530

    fix

--- srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
@@ -2,6 +2,7 @@ package com.sap.bookshop.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.Result;
@@ -12,6 +13,7 @@ import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 
 import cds.gen.bookshop1.Books_;
+import cds.gen.bookshop1.Bookshop1_;
 import cds.gen.adminservice.AdminService_;
 import cds.gen.adminservice.ExternalBooks_;
 @Component
@@ -19,11 +21,22 @@ import cds.gen.adminservice.ExternalBooks_;
 public class AdminServiceHandler implements EventHandler{
 
     @Autowired
-    @Qualifier(Books_.CDS_NAME)
+    @Qualifier(Bookshop1_.CDS_NAME)
     CqnService bupa;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
      @On(entity = ExternalBooks_.CDS_NAME)
   Result readSuppliers(CdsReadEventContext context) {
+    String[] beanNames = applicationContext.getBeanNamesForType(CqnService.class);
+
+    // Print each bean name
+    System.out.println("Beans of type CqnService:");
+    for (String beanName : beanNames) {
+        System.out.println(beanName);
+    }
+    // return null;
     return bupa.run(context.getCqn());
   }
 
