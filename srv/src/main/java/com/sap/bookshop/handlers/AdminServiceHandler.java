cdbeb28de22a8169f79dded682f35984c7a11228
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-07-25 23:01:00 +0530

    Update AdminServiceHandler.java

--- srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/com/sap/bookshop/handlers/AdminServiceHandler.java
@@ -13,14 +13,13 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 
 import cds.gen.bookshop1.Books_;
 import cds.gen.adminservice.AdminService_;
-import cds.gen.adminservice.AdminService;
 import cds.gen.adminservice.ExternalBooks_;
 @Component
 @ServiceName(AdminService_.CDS_NAME)
 public class AdminServiceHandler implements EventHandler{
 
     @Autowired
-    @Qualifier(ExternalBooks_.CDS_NAME)
+    @Qualifier(Books_.CDS_NAME)
     CqnService bupa;
 
      @On(entity = ExternalBooks_.CDS_NAME)
