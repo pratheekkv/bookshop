930308bf5d5eb749e769463d1913fafd543c0759
Pratheek KV <30770392+kvpratheek@users.noreply.github.com>
2024-07-29 10:25:16 +0530

    Add controller

--- srv/src/main/java/com/sap/bookshop/controller/BooksController.java
+++ srv/src/main/java/com/sap/bookshop/controller/BooksController.java
@@ -1,21 +1,119 @@
 package com.sap.bookshop.controller;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sap.cloud.security.client.HttpClientFactory;
+import com.sap.cloud.security.config.OAuth2ServiceConfiguration;
+import com.sap.cloud.security.config.OAuth2ServiceConfigurationBuilder;
+import com.sap.cloud.security.xsuaa.client.DefaultOAuth2TokenService;
+import com.sap.cloud.security.xsuaa.client.OAuth2TokenResponse;
+import com.sap.cloud.security.xsuaa.client.XsuaaDefaultEndpoints;
+import com.sap.cloud.security.xsuaa.tokenflows.XsuaaTokenFlows;
+import com.sap.xs.env.Credentials;
+import com.sap.xs.env.Service;
+import com.sap.xs.env.VcapServices;
+
 import cds.gen.bookshop1.Books;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController("/books")
 public class BooksController {
 
+    private XsuaaTokenFlows xsuaaTokenFlows;
+    private String xsuaaToken;
 
-    List<Books> getBooks(){
-        List<Books> books = new ArrayList<>();
+    @GetMapping("/sdk")
+    public String getBooks(){
+        try{
+            String accessToken = getXSUAAAccessToken();
+            CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        return books;
+            // Create a GET request
+            HttpGet request = new HttpGet("https://rmtest-rmcfkvpr-myspace-bookshop1-srv.cfapps.sap.hana.ondemand.com/odata/v4/Bookshop1/Books");
+
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            // Execute the request
+            CloseableHttpResponse response = httpClient.execute(request);
+
+            // Parse the response
+            String jsonResponse = EntityUtils.toString(response.getEntity());
+
+            response.close();
+            httpClient.close();
+            return jsonResponse;
+        }catch(Exception e){
+            return e.getMessage();
+        }
     }
+
+        private String getXSUAAAccessToken() {
+
+
+        OAuth2TokenResponse tokenResponse = null;
+        final XsuaaTokenFlows tokenFlows = getTokenFlow();
+
+        if (tokenFlows != null) {
+            try {
+                tokenResponse = tokenFlows.clientCredentialsTokenFlow().execute();
+            } catch (final Exception exception) {
+            }
+
+            if (tokenResponse != null) {
+                final String accessToken = tokenResponse.getAccessToken();
+                final String tokenType = tokenResponse.getTokenType();
+                xsuaaToken = tokenType.concat(" ").concat(accessToken);
+                return xsuaaToken;
+            }
+        }
+        return null;
+    }
+
+
+    private XsuaaTokenFlows getTokenFlow(){
+        final VcapServices vcapServices = VcapServices.fromEnvironment();
+        if (vcapServices != null) {
+            final Service service = vcapServices.findService("xsuaa", null, null);
+            if (service != null) {
+                final Credentials credentials = service.getCredentials();
+                final OAuth2ServiceConfigurationBuilder builder = OAuth2ServiceConfigurationBuilder
+                        .forService(com.sap.cloud.security.config.Service.XSUAA);
+                if (credentials != null) {
+                    final OAuth2ServiceConfiguration configuration = getOAuth2ServiceConfiguration(credentials, builder);
+                    xsuaaTokenFlows = new XsuaaTokenFlows(
+                            new DefaultOAuth2TokenService(HttpClientFactory.create(configuration.getClientIdentity())),
+                            new XsuaaDefaultEndpoints(configuration), configuration.getClientIdentity());
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private OAuth2ServiceConfiguration getOAuth2ServiceConfiguration(final Credentials credentials,
+    final OAuth2ServiceConfigurationBuilder builder) {
+        final String clientId = (String) credentials.get("clientid");
+        final String clientSecret = (String) credentials.get("clientsecret");
+        final String certificate = (String) credentials.get("certificate");
+        final String privateKey = (String) credentials.get("key");
+        final String url = (String) credentials.get("url");
+        final String certurl = (String) credentials.get("certurl");
+        return builder.withClientId(clientId).withClientSecret(clientSecret).withCertificate(certificate).withPrivateKey(privateKey)
+                .withUrl(url).withCertUrl(certurl).build();
+}
 
 
 }
